c90e1b291ae79d8502b02d23bd185b698d110582
alexandr <svaluhinav@yandex.ru>
2023-05-09 16:00:47 +0500

    Hello world spring boot 2.7.11

---
+++ src/main/java/com/example/test_hello_world/TestHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestHelloWorldApplication.class, args);
+	}
+
+}
